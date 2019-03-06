e488d77dce28c4b9bd1ebae65224e9ada8038800
Aaron Adelson <aaronsadelson@gmail.com>
2019-03-05 17:42:07 -0800

    add light changing steps to example app

--- examples/index.js
+++ examples/index.js
@@ -4,12 +4,37 @@ const QuickgRPC = require('quick-grpc')
 async function go () {
   const { oakLights } = await new QuickgRPC({
     host: 'localhost:9100',
-    basePath: join(__dirname, '..', '..', 'oak-lights')
+    basePath: join(__dirname, '..')
   })
   let lights = await oakLights()
   lights.info(undefined, function (err, data) {
     if (err) throw err
-    console.log(JSON.stringify(data, null, 2))
+    console.log('Oak Lights Info:', JSON.stringify(data, null, 2))
+    if (!data.controllers.length) {
+      console.log('No light controllers found')
+    } else {
+      const lightControllerId = data.controllers[0].controllerId
+      let req = {
+        controllerId: lightControllerId,
+        hex: '#AA0000',
+        duration: 1000
+      }
+      console.log('ChangeColor Request:', req)
+      lights.ChangeColor(req, function (err, data) {
+        if (err) throw err
+        setTimeout(() => {
+          let req = {
+            controllerId: lightControllerId,
+            hex: '#00AA00',
+            duration: 1000
+          }
+          console.log('ChangeColor Request:', req)
+          lights.ChangeColor(req, function (err, data) {
+            if (err) throw err
+          })
+        }, 2000)
+      })
+    }
   })
 }
 
